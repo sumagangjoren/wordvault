0c7bd1deb56879e9592b50592ca9e6b7d38ef05c
Joren Sumagang <sumagangjoren@gmail.com>
2025-12-08 23:25:45 +0800

    update

--- client/src/layouts/authlayout.jsx
+++ client/src/layouts/authlayout.jsx
@@ -7,7 +7,7 @@ const AuthLayout = () => {
                 <nav aria-label="Global" className="flex items-center justify-between p-6 lg:px-8">
                     <div className="flex lg:flex-1">
                         <NavLink to="/" className="-m-1.5 p-1.5">
-                            <span className="sr-only">Your Company</span>
+                            <span className="sr-only">TITLE </span>
                             <img
                                 alt=""
                                 src="https://tailwindcss.com/plus-assets/img/logos/mark.svg?color=indigo&shade=500"
